d7dc4d90721912e8bd878a2efeafae35756f2892
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-04-05 10:29:25 +0200

    Add initial data seeding for Books_Authors table.

---
+++ SeedData/Insert_Books_Authors.sql
@@ -0,0 +1,12 @@
+INSERT INTO Books_Authors (BookID, AuthorID, Role) 
+VALUES 
+(1, 1, 'Author'),
+(2, 2, 'Author'),
+(3, 3, 'Author'),
+(4, 4, 'Author'),
+(5, 5, 'Author'),
+(6, 6, 'Author'),
+(7, 7, 'Author'),
+(8, 8, 'Author'),
+(9, 9, 'Author'),
+(10, 10, 'Author');
